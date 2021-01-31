56718a67232375a901cbd882d962e1b29f7a11ed
Makombe93 <mak2000@byui.edu>
2021-01-31 02:17:42 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -7,3 +7,32 @@ document.querySelector("#last_modified").innerHTML = lastModified;
 var year = new Date().getFullYear();
 // sends current year to <span id="year"></span> in footer
 document.querySelector("#year").innerHTML = year;
+
+
+//date last modified
+let updateDate = new Date();
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+let weekDay = days[updateDate.getDay()];
+let month = months[updateDate.getMonth()];
+let dayOfMonth = updateDate.getDate();
+let year = updateDate.getUTCFullYear();
+
+document.getElementById("dateModified").innerHTML = weekDay + ", " + month + " " + dayOfMonth + " " + year;
+
+//copyright current year
+let myDate = new Date();
+let currentYear = myDate.getFullYear();
+document.getElementById("demo").innerHTML = currentYear;
+
+//toggle menu button
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+
+    if (document.getElementById("primaryNav").className == "hide") {
+        document.getElementById("menuButton").innerHTML = "&#9776;";
+    } else {
+        document.getElementById("menuButton").innerHTML = "&#10006;";
+    }
+}
